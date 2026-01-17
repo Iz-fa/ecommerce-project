2fd298a79a281bf983aca28c7f35afd538ce17c9
Iz-fa <fastol35@gmail.com>
2026-01-17 17:49:00 +0100

    Use data from the backend to generate the html

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,13 +1,24 @@
 import axios from 'axios'; //cleaner way to make requests to the backend
+import {useEffect, useState} from 'react';
 import { Header } from '../components/Header';
-import { products } from '../../starting-code/data/products';
 import './HomePage.css'
 
+// Backend stores the data(normaly on a different computer) so the user's computer doesnt have to store all the
+// products on their computer(It fetches only the products it needs to display)
+
+// This also helps when adding an item to the cart, so when the user uses a different computer, the cart would
+// still have the items inside
+
 export function HomePage() {
-    axios.get('http://localhost:3000/api/products')      //this is an easier way instead of fetch .. .then response.json().then
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=>{   //the <StrictMode> in main.jsx makes useEffect run twice
+        axios.get('http://localhost:3000/api/products')      //this is an easier way instead of fetch .. .then response.json().then
         .then((response)=>{
-            console.log(response.data); 
+            setProducts(response.data); 
         });
+    }, []);
+    
 
     return (
         <>
