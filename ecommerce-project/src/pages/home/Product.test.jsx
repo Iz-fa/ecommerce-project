b75a2efa668300b9cb8efd33a634d8180a125771
Iz-fa <fastol35@gmail.com>
2026-01-20 15:22:37 +0100

    Test clicking add to cart button

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,8 +1,12 @@
 // This is an Integration test
 
-import { it, expect, describe, vi } from 'vitest'; //vi lets us create a mock
+import { it, expect, describe, vi } from 'vitest'; //vi lets us create a mock (a fake function that doesnt do anything)
 import { render, screen } from '@testing-library/react'; //render: renders a component in a fake webpage(for testing), screen: lets us check the fake webpage
+import userEvent from '@testing-library/user-event'; // userEvent lets us simulate events(like clicking a button)
+import axios from 'axios';
 import { Product } from './Product';
+
+vi.mock('axios'); //mocking the entire axios package lets us use a fake version of axios
 
 describe('Product component', () => {
     it('displays the product details correctly', () => {
@@ -43,6 +47,41 @@ describe('Product component', () => {
         expect(
             screen.getByText('87')
         ).toBeInTheDocument();
+    });
+
+    it('adds a product to the cart', async ()=>{
+        const product = {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+                stars: 4.5,
+                count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        };
+        const loadCart = vi.fn(); 
+
+
+        render(<Product product={product} loadCart ={loadCart} />);
+
+        const user = userEvent.setup();
+
+        const addToCartButton = screen.getByTestId('add-to-cart-button');
+        await user.click(addToCartButton);
+
+        //even though mocks dont do anything, we can still make sure we called them and gave them the correct values
+        expect(axios.post).toHaveBeenCalledWith(
+            '/api/cart-items',
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1
+            }
+        );
+
+        expect(loadCart).toHaveBeenCalled();
+
     });
 });
 
