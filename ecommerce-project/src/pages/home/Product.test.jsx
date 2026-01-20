ecfbf5b48309efc3f8e93d70dcadbe5e141ee63d
Iz-fa <fastol35@gmail.com>
2026-01-20 17:06:39 +0100

    use beforeEach to remove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,6 +1,6 @@
 // This is an Integration test
 
-import { it, expect, describe, vi } from 'vitest'; //vi lets us create a mock (a fake function that doesnt do anything)
+import { it, expect, describe, vi, beforeEach } from 'vitest'; //vi lets us create a mock (a fake function that doesnt do anything)
 import { render, screen } from '@testing-library/react'; //render: renders a component in a fake webpage(for testing), screen: lets us check the fake webpage
 import userEvent from '@testing-library/user-event'; // userEvent lets us simulate events(like clicking a button)
 import axios from 'axios';
@@ -9,8 +9,13 @@ import { Product } from './Product';
 vi.mock('axios'); //mocking the entire axios package lets us use a fake version of axios
 
 describe('Product component', () => {
-    it('displays the product details correctly', () => {
-        const product = {
+    let product;
+    let loadCart;
+
+    //This is a test hook, there are others like afterEach,beforeAll,afterAll
+    //It means before each test run the code inside it
+    beforeEach(()=>{
+        product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -24,8 +29,10 @@ describe('Product component', () => {
 
         // This creates a mock (a fake function that doesnt do anything)
         // because we cant use the real loadCart() since it contacts the backend
-        const loadCart = vi.fn(); 
+        loadCart= vi.fn();
+    });
 
+    it('displays the product details correctly', () => {
         render(<Product product={product} loadCart ={loadCart} />);
 
         expect(
@@ -50,20 +57,6 @@ describe('Product component', () => {
     });
 
     it('adds a product to the cart', async ()=>{
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        };
-        const loadCart = vi.fn(); 
-
-
         render(<Product product={product} loadCart ={loadCart} />);
 
         const user = userEvent.setup();
