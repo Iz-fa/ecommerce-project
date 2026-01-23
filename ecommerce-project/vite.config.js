6bf636fc5b3f39923a4e253de29afd08d2d1c12b
Iz-fa <fastol35@gmail.com>
2026-01-23 15:17:58 +0100

    Seting up the react compiler

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+    },
+  })],
   server: {
     proxy: {
       '/api': {  //this means if the url starts with /api, it will automaticaly go to the target
